abfa5e2a5bc00218b9d1950fe0947bf98868d00b
cxcx <77737876+chong-xin1230@users.noreply.github.com>
2024-01-08 22:10:42 +0800

    Update and rename 3.c to 基礎3.c

--- 基礎3.c
+++ 基礎3.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     int N;
     int sum = 0;
     scanf("%d", &N);
-    for (int i = 1; i <= N; ++i) {
-        if (i % 3 == 0) {
+    for (int i = 1; i <= N; ++i){
+        if (i % 3 == 0){
             sum += i;
         }
     }
